2c0b46f822eed8fa93d0d18a70f4a1891935bd98
VirjanandI <110103050+VirjanandI@users.noreply.github.com>
2022-08-11 16:39:09 +0530

    Add files via upload

---
+++ Perform Calculation( if statement ).cs
@@ -0,0 +1,41 @@
+﻿using System;
+class PerformCalculation
+{
+    static int Calculation(int num1, int num2)
+    {
+        Console.WriteLine("Enter Operator : ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        int result = 0;
+        if (op == '+')
+            result = num1 + num2;
+        else if (op == '-')
+            result = num1 - num2;
+
+        else if (op == '*')
+            result = num1 * num2;
+
+        else if (op == '/')
+            result = num1 / num2;
+        else
+            result = -33;
+
+        return result;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter the number : ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number : ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int a = Calculation(num1, num2);
+	
+	if(a<0)
+		Console.WriteLine("Invalid operator")
+	else
+		Console.WriteLine(a)
+    }
+}
+
---
+++ PrintPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class PrintPattern
+{
+    static void Pattern()
+    {
+        int ctr = 1;
+        while (ctr <= 3)
+        {
+            int ptr = 1;
+            while (ptr <= ctr)
+            {
+                Console.Write("A");
+                ptr++;
+            }
+            Console.WriteLine();
+            ctr++;
+        }
+    }
+    static void Main2()
+    {
+        Pattern();
+    }
+
+}
